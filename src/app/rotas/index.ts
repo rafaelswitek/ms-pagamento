6aaa57b0862aa542c1fe7313f11661f026cf1212
Rafael Gonçalves <rafaelswitek@gmail.com>
2024-01-09 02:29:06 -0300

    Alterando configurações de rota

--- src/app/rotas/index.ts
+++ src/app/rotas/index.ts
@@ -1,6 +1,15 @@
 import express from "express";
-import pagamentosRotas from "../rotas/pagamentosRotas";
+import PagamentoRepository from "../../infra/repositories/PagamentoRepository";
+import { AppDataSource } from "../../infra/config/dataSource";
+import CriarPagamentoUseCase from "../../domain/useCases/CriarPagamentoUseCase";
+import PagamentoController from "../controllers/PagamentoController";
 const router = (app: express.Router) => {
-  app.use("/pagamentos", pagamentosRotas);
+    const pagamentoRepository = new PagamentoRepository(
+        AppDataSource.getRepository("Pagamento")
+    );
+    const useCase = new CriarPagamentoUseCase(pagamentoRepository);
+    const pagamentoController = new PagamentoController(useCase);
+
+    app.post("/pagamento/criar", (req, res) => pagamentoController.criaPagamento(req, res));
 };
 export default router;
