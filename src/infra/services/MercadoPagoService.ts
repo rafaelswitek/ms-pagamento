8ebc6a263f00715c595fad4629346acde79de7fb
Rafael Gonçalves <rafaelswitek@gmail.com>
2024-01-14 17:31:41 -0300

    usando o adapter

--- src/infra/services/MercadoPagoService.ts
+++ src/infra/services/MercadoPagoService.ts
@@ -1,45 +1,34 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 import { PurchaseDto } from '../../app/dtos/mercadoPago.dto'
+import HttpClient from '../../domain/interfaces/HttpClient'
 
 class MercadoPagoService {
   private readonly apiUrl: string
-  private readonly apiKey: string
   private readonly userId: string
   private readonly posId: string
+  private readonly config: any
 
-  constructor() {
+  constructor(private httpClient: HttpClient) {
     this.apiUrl = 'https://api.mercadopago.com'
-    this.apiKey = process.env.MERCADO_PAGO_KEY!
     this.userId = process.env.MERCADO_PAGO_USER_ID!
     this.posId = process.env.MERCADO_PAGO_POS_ID!
+
+    const apiKey = process.env.MERCADO_PAGO_KEY!
+    this.config = {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${apiKey}`,
+      },
+    }
   }
 
-  async gerarQrCodeDinamico(data: PurchaseDto): Promise<AxiosResponse> {
+  async gerarQrCodeDinamico(data: PurchaseDto): Promise<any> {
     const fullUrl = `${this.apiUrl}/instore/orders/qr/seller/collectors/${this.userId}/pos/${this.posId}/qrs`
-    const config: AxiosRequestConfig = {
-      method: 'post',
-      url: fullUrl,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${this.apiKey}`,
-      },
-      data: JSON.stringify(data),
-    }
-
-    return axios.request(config)
+    const resposta = await this.httpClient.post(fullUrl, JSON.stringify(data), this.config)
+    return resposta
   }
 
-  async consultarStatus(url: string): Promise<AxiosResponse> {
-    const config: AxiosRequestConfig = {
-      method: 'get',
-      url: url,
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${this.apiKey}`,
-      },
-    }
-
-    return axios.request(config)
+  async consultarStatus(url: string): Promise<any> {
+    return this.httpClient.get(url, this.config)
   }
 }
 
