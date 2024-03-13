01f9074f7cd131a36694a5334673fdee86ef7a09
Rafael Gonçalves <rafael.goncalves@emitte.com.br>
2024-03-12 23:42:07 -0300

    Ajustes OWASP ZAP

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,23 @@ import express from 'express'
 import 'reflect-metadata'
 import { AppDataSource } from './infra/config/dataSource'
 import rotas from './app/rotas'
+
 const app = express()
+
+var helmet = require('helmet');
+app.use(helmet({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+    },
+  },
+}));
+
+app.disable('x-powered-by')
+
 app.use(express.json())
+
 rotas(app)
 
 AppDataSource.initialize()
